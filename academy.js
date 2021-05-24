7a13c286fc2b61deb955216b6a9f68588760bea7
mariya <mariya.podosinova@infinityworks.com>
2021-05-24 17:07:02 +0100

    refactor gameOn

--- academy.js
+++ academy.js
@@ -32,33 +32,31 @@ function checkWinner() {
   // console.log(numTurns);
   console.log(boardToArray);
 
-  if (numTurns > 4) {
-    if (boardToArray[0] && boardToArray[1] && boardToArray[2]) {
-      winner = boardToArray[0] === "nought" ? "noughts" : "crosses";
-      gameOn = false;
-      return winner;
-    } else if (boardToArray[3] && boardToArray[4] && boardToArray[5]) {
-      gameOn = false;
-      return boardToArray[3] === "nought" ? "noughts" : "crosses";
-    } else if (boardToArray[6] && boardToArray[7] && boardToArray[8]) {
-      gameOn = false;
-      return boardToArray[6] === "nought" ? "noughts" : "crosses";
-    } else if (boardToArray[0] && boardToArray[3] && boardToArray[6]) {
-      gameOn = false;
-      return boardToArray[0] === "nought" ? "noughts" : "crosses";
-    } else if (boardToArray[1] && boardToArray[4] && boardToArray[7]) {
-      gameOn = false;
-      return boardToArray[1] === "nought" ? "noughts" : "crosses";
-    } else if (boardToArray[2] && boardToArray[5] && boardToArray[8]) {
-      gameOn = false;
-      return boardToArray[2] === "nought" ? "noughts" : "crosses";
-    } else if (boardToArray[0] && boardToArray[4] && boardToArray[8]) {
-      gameOn = false;
-      return boardToArray[0] === "nought" ? "noughts" : "crosses";
-    } else if (boardToArray[6] && boardToArray[4] && boardToArray[2]) {
-      gameOn = false;
-      return boardToArray[6] === "nought" ? "noughts" : "crosses";
-    }
+  if (boardToArray[0] && boardToArray[1] && boardToArray[2]) {
+    winner = boardToArray[0] === "nought" ? "noughts" : "crosses";
+    gameOn = false;
+    return winner;
+  } else if (boardToArray[3] && boardToArray[4] && boardToArray[5]) {
+    gameOn = false;
+    return boardToArray[3] === "nought" ? "noughts" : "crosses";
+  } else if (boardToArray[6] && boardToArray[7] && boardToArray[8]) {
+    gameOn = false;
+    return boardToArray[6] === "nought" ? "noughts" : "crosses";
+  } else if (boardToArray[0] && boardToArray[3] && boardToArray[6]) {
+    gameOn = false;
+    return boardToArray[0] === "nought" ? "noughts" : "crosses";
+  } else if (boardToArray[1] && boardToArray[4] && boardToArray[7]) {
+    gameOn = false;
+    return boardToArray[1] === "nought" ? "noughts" : "crosses";
+  } else if (boardToArray[2] && boardToArray[5] && boardToArray[8]) {
+    gameOn = false;
+    return boardToArray[2] === "nought" ? "noughts" : "crosses";
+  } else if (boardToArray[0] && boardToArray[4] && boardToArray[8]) {
+    gameOn = false;
+    return boardToArray[0] === "nought" ? "noughts" : "crosses";
+  } else if (boardToArray[6] && boardToArray[4] && boardToArray[2]) {
+    gameOn = false;
+    return boardToArray[6] === "nought" ? "noughts" : "crosses";
   }
 
   console.log("checkWinner was called");
@@ -71,6 +69,7 @@ function checkWinner() {
 // Set the game state back to its original state to play another game.
 function resetGame() {
   console.log("resetGame was called");
+  gameOn = true;
   board = [
     [null, null, null],
     [null, null, null],
@@ -85,25 +84,6 @@ function resetGame() {
 function getBoard() {
   return board;
 }
-
-// Set the game state back to its original state to play another game.
-function resetGame() {
-  console.log("resetGame was called");
-  board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
-  return board;
-}
-
-// Return the current board state with either a "nought" or a "cross" in
-// each position. Put a null in a position that hasn't been played yet.
-
-function getBoard() {
-  return board;
-}
-
 if (typeof exports === "object") {
   console.log("Running in Node");
   // Node. Does not work with strict CommonJS, but only CommonJS-like
